78d7284f8eee4be7537f5446e37e85da3bd78da2
PeterFineTheAgency <peter.chen@finetheagency.com>
2017-05-23 11:14:35 -0700

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoogse} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -28,6 +29,30 @@ app.get('/todos', (req,res)=>{
   },(e)=>{
     res.status(400).send(e);
   })
+})
+
+app.get('/todos/:id',(req,res)=>{
+  var id = req.params.id;
+
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+  //404
+  //find by id
+  Todo.findById(id).then((todo)=>{
+    if(!todo){
+      return res.status(404).send();
+    }
+  res.send({todo});
+  }).catch((e)=>{
+    return res.status(400).send(e);
+  })
+  //success
+  //if todo send back
+  //if not send back 404
+  //error
+  //400
+  //res.send(req.parms.id);
 })
 
 app.listen(3000, ()=>{
