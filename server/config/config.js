142644c994b3d49ce955e4268d3571f57ac38937
PeterFineTheAgency <peter.chen@finetheagency.com>
2017-05-24 11:17:03 -0700

    fix config.js

--- server/config/config.js
+++ server/config/config.js
@@ -5,7 +5,7 @@ console.log('env *******' + env);
 if(env === 'development'){
   process.env.PORT = 3000;
   process.env.MONGODB_URL = 'mongodb://localhost:27017/TodoApp';
-} else if(env = 'test'){
+} else if(env === 'test'){
   process.env.PORT = 3000;
   process.env.MONGODB_URL = 'mongodb://localhost:27017/TodoAppTest';
 } else {
