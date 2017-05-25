bf5d226ae6f21c85d11d2e8f181e36784c7deb85
PeterFineTheAgency <peter.chen@finetheagency.com>
2017-05-25 14:37:58 -0700

    Update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -2,12 +2,16 @@ var env = process.env.NODE_ENV || 'development';
 
 console.log('env *******' + env);
 
-if(env === 'development'){
-  process.env.PORT = 3000;
-  process.env.MONGODB_URL = 'mongodb://localhost:27017/TodoApp';
-} else if(env === 'test'){
-  process.env.PORT = 3000;
-  process.env.MONGODB_URL = 'mongodb://localhost:27017/TodoAppTest';
-} else {
-  process.env.MONGODB_URL = 'mongodb://rulerpe:ddddffff@ds149431.mlab.com:49431/nodetodoapiapp';
+if(env === 'development' || env == 'test'){
+  var config = require('./config.json');
+  var envConfig = config[env];
+
+  Object.keys(envConfig).forEach((key)=>{
+    process.env[key] = envConfig[key];
+  })
 }
+
+// else {
+//   process.env.MONGODB_URL = 'mongodb://rulerpe:ddddffff@ds149431.mlab.com:49431/nodetodoapiapp';
+// }
+
